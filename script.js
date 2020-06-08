50d4119157a57fd6b089ea133462952b69f01633
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-06-09 00:18:49 +1000

    started adding Script to to the buttons and answers

---
+++ script.js
@@ -0,0 +1,63 @@
+
+const questionText=document.querySelector(".question-text");
+const optionBox=document.querySelector(".option-box");
+const questionIndex=0;
+//question and options and answers
+//array of objects
+myApp=[
+    {
+        question: "Can you name two programming paradigms important for JavaScript app developers?",
+        options: ["Prototypal and Functional", "Directional and Prototypal", "Functional and Directional","Directional and flow"],
+        answer: 0,
+    },
+    {
+        question: "Select one answer that identifys with Functional Programming?",
+        options: ["Hard function compostion", "Pure functions and function purity", "feature of Javascript","side effects"],
+        answer: 1,  
+    },
+    {
+        question: "Select one answer that identifys with favor object composition over class inheritance mean?",
+        options: ["tight coupling", "brittle base class", "Aviod class hierarchies","rigid taxonomy"],
+        answer: 2,  
+    },
+    {
+        question: "How can you detect the client's browser name?",
+        options: ["client.navName", "navigator.appName", "browser.name","web.name"],
+        answer: 1,  
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        options: ["onmouseover", "onmouseclick", "onchange","onclick"],
+        answer:3,  
+    },
+]
+
+function load(){
+    questionText.innerHTML=myApp[questionIndex].question;
+    createOptions();
+}
+
+function createOptions(){
+    for(let i =0; i < myApp[questionIndex].options.length; i++){
+        const option=document.createElement("div");
+        option.innerHTML=myApp[questionIndex].options[i];
+        option.classList.add("option"); 
+        option.id=i;
+        option.setAttribute("onclick", "check(this)");
+        optionBox.appendChild(option);
+    }
+}
+
+function check(ele){
+     const id=ele.id;
+     if(id == myApp[questionIndex].answer){
+        ele.classList.add("correct");
+     }
+     else {
+        ele.classList.add("wrong");
+     }
+}
+
+window.onload=()=>{
+    load();
+}
